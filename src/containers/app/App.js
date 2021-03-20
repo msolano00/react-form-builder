da322e9cfa6cecc5b222903656f1a9fc08f62122
Marvin <marvin_solano@mckinsey.com>
2021-03-20 19:23:34 -0400

    Enhance presentation

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -1,11 +1,34 @@
-import './App.css';
+import React, { useState } from 'react';
 import Form from '../../components/form';
 import configuration from './configuration.json';
+import './App.css';
 
-function App() {
+const App = () => {
+
+  const stateBuilder = configuration.form.map(element => ({ id: element.configuration.id, value: '' }));
+  const [formData, setFormData] = useState(stateBuilder);
+
   return (
     <div className="App">
-      <Form configuration={configuration}/>
+      <div className="row">
+        <div className="column">
+          <Form 
+            configuration={configuration} 
+            formState={formData}
+            handleFormChange={setFormData}
+          />
+          <div className="output">
+            <pre>
+              {JSON.stringify(formData, null, 2)}
+            </pre>
+          </div>
+        </div>
+        <div className="column configuration">
+          <pre>
+            {JSON.stringify(configuration, null, 2)}
+          </pre>
+        </div>
+      </div>
     </div>
   );
 }
